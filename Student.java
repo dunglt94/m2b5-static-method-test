c6135ce6fd7d8812677bcaa049b454461b438e7e
dunglt94 <tuandung9594@gmail.com>
2024-10-15 14:42:31 +0700

    first add

---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+
+    public Student() {}
+    public Student(int rollno, String name) {
+        this.rollno = rollno;
+        this.name = name;
+    }
+
+    static void change() {
+        college = "CODEGYM";
+    }
+
+    void display() {
+        System.out.println(rollno + " " + name + " " + college);
+    }
+}
